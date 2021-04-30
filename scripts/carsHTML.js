96fcd4338074749dac06a56ac974d7060282c2da
Dakota Lambert <dakotalambertbiz@gmail.com>
2021-04-30 16:52:45 -0400

    HTML wireframe complete

---
+++ scripts/carsHTML.js
@@ -0,0 +1,53 @@
+//*Why this module? Why house HTML here instead of main.js?
+
+//* this module builds the html frame, to later be rendered in another module
+
+import { interiors } from "./interior.js";
+import { colors } from "./paint.js"
+import { techPacks } from "./tech.js";
+import { Wheels } from "./wheels.js";
+import { Orders } from "./orders.js";
+
+
+export const hauntedCarsHTML = () => {
+  return ` 
+  
+  <h1>Haunted Lit-Cars</h2>
+
+
+<article class="options">
+    <section>
+    <h2>Paint Color</h2>
+        ${ colors() }
+    </section>
+    
+    
+    <section>
+    <h2>Interior Style</h2>
+        ${ interiors() }
+    </section>
+    
+    
+    <section>
+    <h2>Tech Package</h2>
+        ${ techPacks() }
+    </section>
+    
+    
+    <section>
+    <h2>Wheels</h2>
+    ${ Wheels() }
+    </section>
+  
+</article>
+
+<article>
+    <button>Create Order</button>
+    </article>
+
+    <article>
+    <h2> Orders </h2>
+    ${Orders()}
+</article>
+`;
+};
